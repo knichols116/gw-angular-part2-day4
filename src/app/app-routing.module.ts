dc256dcc91b983275ffa1e52846cc63dedfc77bd
Korey Nichols <knichols116@hotmail.com>
2019-07-10 19:24:38 -0700

    day 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'profile/:id', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
